d915df041f9a1cba610779f858621b54283379e9
Oskar Hahn <mail@oshahn.de>
2024-12-04 23:43:02 +0100

    Fix realloc (#6)
    Also add a nicer error message on OOM and update the roc stdlib

--- host/RocAllocator.zig
+++ host/RocAllocator.zig
@@ -13,9 +13,9 @@ pub fn alloc(self: this, size: usize, alignment: u32) ![*]u8 {
 }
 
 fn alloc_with_len(self: this, size: usize, alignment: u32) ![*]u8 {
-    const zig_alignment: usize = if (alignment <= 8) 8 else 16;
+    const zig_alignment: u32 = if (alignment <= 8) 8 else 16;
     const size_with_len = size + zig_alignment;
-    const ptr = try self.alloc_without_len(size_with_len, alignment);
+    const ptr = try self.alloc_without_len(size_with_len, zig_alignment);
     const as_usize: [*]usize = @ptrCast(@alignCast(ptr));
     as_usize[0] = size;
     return ptr + zig_alignment;
@@ -33,32 +33,31 @@ fn alloc_without_len(self: this, size: usize, alignment: u32) ![*]u8 {
 }
 
 pub fn realloc(self: this, ptr: [*]u8, new_size: usize, old_size: usize, alignment: u32) ![*]u8 {
-    const zig_alignment: usize = if (alignment <= 8) 8 else 16;
-    const slice = if (self.skip_deallocate) ptr[0..old_size] else (ptr - zig_alignment)[0 .. old_size + zig_alignment];
+    const zig_alignment: u32 = if (alignment <= 8) 8 else 16;
     const real_new_size = if (self.skip_deallocate) new_size else new_size + zig_alignment;
+    const slice = if (self.skip_deallocate) ptr[0..old_size] else (ptr - zig_alignment)[0 .. old_size + zig_alignment];
 
-    if (self.allocator.resize(slice, real_new_size)) {
-        if (!self.skip_deallocate) {
-            const size_pointer: [*]usize = @ptrCast(@alignCast(ptr - zig_alignment));
-            size_pointer[0] = new_size;
-        }
-        return ptr;
-    }
+    const slice_aligned = if (alignment <= 8)
+        @as([]align(8) u8, @ptrCast(@alignCast(slice)))
+    else
+        @as([]align(16) u8, @ptrCast(@alignCast(slice)));
 
-    const new_ptr = try self.alloc(new_size, alignment);
-    const copy_size = @min(old_size, new_size);
-    @memcpy(new_ptr[0..copy_size], ptr[0..copy_size]);
-
-    self.dealloc(ptr, alignment);
-    return new_ptr;
+    const new_slice = try self.allocator.realloc(slice_aligned, real_new_size);
+    return new_slice.ptr + zig_alignment;
 }
 
 pub fn dealloc(self: this, ptr: [*]u8, alignment: u32) void {
     if (self.skip_deallocate) return;
 
-    const zig_alignment: usize = if (alignment <= 8) 8 else 16;
+    const zig_alignment: u32 = if (alignment <= 8) 8 else 16;
     const size_pointer: [*]usize = @ptrCast(@alignCast(ptr - zig_alignment));
     const size = size_pointer[0];
     const real_size = size + zig_alignment;
-    self.allocator.free(@as([*]u8, @ptrCast(size_pointer))[0..real_size]);
+
+    const slice = if (alignment <= 8)
+        @as([*]align(8) u8, @ptrCast(@alignCast(size_pointer)))[0..real_size]
+    else
+        @as([*]align(16) u8, @ptrCast(@alignCast(size_pointer)))[0..real_size];
+
+    self.allocator.free(slice);
 }
